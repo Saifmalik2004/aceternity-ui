ad59c42d4741ec480121a677621a3210386f8e68
saifmalik2004 <saifmalik7827@gmail.com>
2024-08-24 18:59:27 +0530

    add carhover page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import Herosection from "@/components/Herosection";
 import MusicSchoolTestimonials from "@/components/TestimonialCard";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -11,6 +12,7 @@ export default function Home() {
     <FeaturedSection/>
     <WhyChooseUs/>
     <MusicSchoolTestimonials/>
+    <UpcomingWebinar/>
    </main>
   );
 }
